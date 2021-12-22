4bf7584d633ccad7df97c1b57b91d8f15b36bd1e
Renee <circle81918@gmail.com>
2021-12-22 22:57:43 +0800

    feat: add basic source file

---
+++ InjectHook/InjectHook.cpp
@@ -0,0 +1,119 @@
+﻿#include <iostream>
+#include <string>
+#include <ctype.h>
+#include <Windows.h>
+#include <tlhelp32.h>
+#include <Shlwapi.h>
+//Library needed by Linker to check file existance
+#pragma comment(lib, "Shlwapi.lib")
+
+using namespace std;
+
+//-----------------------------------------------------------
+// Inject DLL to target process
+//-----------------------------------------------------------
+bool InjectDLLWithPID(const int& pid, const string& DLL_Path)
+{
+  HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+
+  if (hProc == NULL)
+  {
+    cout << "Fail to open target process!" << endl;
+    return false;
+  }
+  
+  do
+  {
+    LPVOID MyAlloc = VirtualAllocEx(hProc, NULL, DLL_Path.length(), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    if (MyAlloc == NULL)
+    {
+      cout << "Fail to allocate memory in Target Process." << endl;
+      break;
+    }
+
+    int IsWriteOK = WriteProcessMemory(hProc, MyAlloc, DLL_Path.c_str(), DLL_Path.length(), 0);
+    if (IsWriteOK == 0)
+    {
+      cout << "Fail to write in Target Process memory." << endl;
+      break;
+    }
+
+    LPTHREAD_START_ROUTINE addrLoadLibrary = (LPTHREAD_START_ROUTINE)GetProcAddress(LoadLibrary("kernel32"), "LoadLibraryA");
+    HANDLE ThreadReturn = CreateRemoteThread(hProc, NULL, 0, addrLoadLibrary, MyAlloc, 0, NULL);
+    if (ThreadReturn == NULL)
+    {
+      cout << "Fail to create Remote Thread" << endl;
+      break;
+    }
+
+    CloseHandle(ThreadReturn);
+    CloseHandle(hProc);
+    return true;
+  }while(0);
+
+  return false;
+}
+
+//-----------------------------------------------------------
+// Get All Process ID by its name
+//-----------------------------------------------------------
+int InjectDLLWithProcName(const string& p_name, const string& DLL_Path)
+{
+  HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+  PROCESSENTRY32 structprocsnapshot = { 0 };
+
+  structprocsnapshot.dwSize = sizeof(PROCESSENTRY32);
+
+  if (snapshot == INVALID_HANDLE_VALUE)
+    return 0;
+  if (Process32First(snapshot, &structprocsnapshot) == FALSE)
+    return 0;
+
+  while (Process32Next(snapshot, &structprocsnapshot))
+  {
+    if (!strcmp(structprocsnapshot.szExeFile, p_name.c_str()))
+    {
+      cout << "Process name is: " << p_name << "\nProcess ID: " << structprocsnapshot.th32ProcessID << endl;
+      if (!InjectDLLWithPID(structprocsnapshot.th32ProcessID, DLL_Path))
+      {
+        CloseHandle(snapshot);
+        cout << "Inject process with Process ID:" << structprocsnapshot.th32ProcessID << "fail!" << endl;
+        return -1;
+      }
+    }
+  }
+  CloseHandle(snapshot);
+  cout << "Unable to find Process ID" << endl;
+  return 0;
+}
+
+void usage()
+{
+  cout << "Usage: DLL_Injector.exe <Process name | Process ID> <DLL Path to Inject>" << endl;
+}
+
+int main(int argc, char** argv)
+{
+  if (argc != 3)
+  {
+    usage();
+    return EXIT_FAILURE;
+  }
+
+  if (PathFileExists(argv[2]) == FALSE)
+  {
+    cout << "DLL file does NOT exist!" << endl;
+    return EXIT_FAILURE;
+  }
+
+  if (isdigit(argv[1][0]))
+  {
+    InjectDLLWithPID(atoi(argv[1]), argv[2]);
+  }
+  else 
+  {
+    InjectDLLWithProcName(argv[1], argv[2]);
+  }
+
+  return EXIT_SUCCESS;
+}
---
+++ apihook/dllmain.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <Psapi.h>
+#include "../minhook/include/MinHook.h"
+
+#pragma comment(lib, "libMinHook.x64.lib")
+
+typedef BOOL (WINAPI* pSetWindowText)(HWND hWnd, LPCWSTR lpString);
+pSetWindowText originSetWindowText = nullptr;
+
+BOOL WINAPI DetourSetWindowTextW(HWND hWnd, LPCWSTR lpString) {
+  OutputDebugStringA("hook detour");
+  OutputDebugStringW(lpString);
+  return originSetWindowText(hWnd, lpString);
+}
+
+BOOL SetExportHook(LPVOID originFuncAddress, LPVOID detourFunc, LPVOID originFunc) {
+  if (MH_CreateHook(originFuncAddress, detourFunc, reinterpret_cast<LPVOID*>(originFunc)) != MH_OK) {
+    OutputDebugStringA("CreateHook fail");
+    return false;
+  }
+
+  if (MH_EnableHook(originFuncAddress) != MH_OK) {
+    OutputDebugStringA("EnableHook fail");
+    return false;
+  }
+
+  return true;
+}
+
+BOOL SetUnexportHook() {
+  HMODULE hModule = GetModuleHandleA("chrome.dll");
+  if(!hModule)
+    OutputDebugStringA("Cannot find chrome.dll");
+
+  unsigned int search_offset_start = 0x5952cc0;
+  unsigned int search_offset_end = 0x4000;
+  unsigned char pattern_header[] = {0x41, 0x57, 0x41, 0x56, 0x41, 0x54, 0x56, 0x57, 0x55, 0x53, 0x48, 0x83, 0xec, 0x30, 0xc6, 0x02, 0x0, 0x48, 0x8b, 0x41, 0x30, 0x83, 0xb8, 0xac, 0x0, 0x0, 0x0, 0x0};
+  unsigned int pattern_header_len = sizeof(pattern_header);
+  unsigned char pattern_tail[] = {0xb8, 0xff, 0xff, 0xff, 0xff, 0x48, 0x83, 0xc4, 0x30, 0x5b, 0x5d, 0x5f, 0x5e, 0x41, 0x5c, 0x41, 0x5e, 0x41, 0x5f, 0xc3};
+  unsigned int pattern_tail_len = sizeof(pattern_tail);
+  unsigned int pattern_ignore_len = 35;
+
+  
+  PBYTE StartSearchOffset = (PBYTE)hModule + search_offset_start;
+  PBYTE EndSearchOffset = (PBYTE)hModule + search_offset_start + search_offset_end;
+
+  for (PBYTE pbyTmp = StartSearchOffset; pbyTmp < EndSearchOffset; pbyTmp++)
+  {
+    if (memcmp(pbyTmp, pattern_header, pattern_header_len) == 0)
+    {
+      if (memcmp(pbyTmp + pattern_header_len + pattern_ignore_len, pattern_tail, pattern_tail_len) != 0)
+        continue;
+
+      OutputDebugStringA("Success Hook unexport tls_write_app_data ");
+      break;
+    }
+  }
+  OutputDebugStringA("Fail Hook unexport tls_write_app_data ");
+  return true;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+      do {
+        OutputDebugStringA("Start Hook!!");
+
+        if (MH_Initialize() != MH_OK) {
+          OutputDebugStringA("Init fail");
+          break;
+        }
+
+        SetExportHook(&SetWindowTextW, &DetourSetWindowTextW, &originSetWindowText);
+        SetUnexportHook();
+
+      }while(0);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
